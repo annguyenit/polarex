ed9e680f155e20f9ddd82083c492149472ddb608
annguyenit <annguyenphp@gmail.com>
2016-04-17 21:55:52 +0700

    Implement Email template

--- datve.php
+++ datve.php
@@ -8,10 +8,13 @@ include_once("File_control/Connection.php");
 $connect = new Connection;
 $connect->Connect2DB();
 $MAKH='G'.date('dmYHis');
+$MADH = 'MH'.date('dmYHis');
+$ngaydatve = time();
+$giave = explode('x', $diengiai[0]);
+$giave = (int)$giave[1];
 
 $connect->ExecuteDB( 'INSERT INTO users VALUES("'.$MAKH.'","","'.$thongtinkhachhang[2].'","","","'.$thongtinkhachhang[3].' '.$thongtinkhachhang[4].'","'.$thongtinkhachhang[5].'","'.$thongtinkhachhang[0].'")');
- 
-$connect->ExecuteDB('INSERT INTO don_hang VALUES("MH'.date('dmYHis').'","'.$MAKH.'","'.$dondathang[0].'","'.$dondathang[2].'","'.date('Y-m-d').'","0","'.$dondathang[1].'","'.$dondathang[3].'")');
+$connect->ExecuteDB('INSERT INTO don_hang VALUES("'.$MADH.'","'.$MAKH.'","'.$dondathang[0].'","'.$dondathang[2].'","'.date('Y-m-d', $ngaydatve).'","0","'.$dondathang[1].'","'.$dondathang[3].'")');
 
 // Sendmail cho khach hang
 require 'vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
@@ -34,22 +37,25 @@ $mail->AddAddress($thongtinkhachhang[0]);
 $mail->IsHTML(true);                                  // Set email format to HTML
 
 $mail->From = 'info@saigonpolarexpo.com.vn';
-$mail->FromName = 'Sai Gon Polarex';
+$mail->FromName = 'Saigon Polar Expo';
 $mail->Subject = 'Thông tin đặt vé';
 //$mail->mailHeader = 'Content-type: text/html; charset=utf-8';
 
-$body = '
-    Xin chào ! Bạn đã đặt vé thành công <br/>
-    Họ và tên: '.$thongtinkhachhang[2].' <br/>
-    Email: '.$thongtinkhachhang[0].' <br/>
-    Địa chỉ: '.$thongtinkhachhang[3].' <br/>
-    Thành phố: '.$thongtinkhachhang[4].' <br/>
-    Số điện thoại: '.$thongtinkhachhang[5].' <br/>
-    Số lượng vé đặt: '.$dondathang[2].' <br/>
-    Loại vé: '.$dondathang[3].' <br/>
-    Tổng tiền: '.$dondathang[1].' <br/>
-    Xin cảm ơn !
-';
+require 'emailTemplate.php';
+$template = new EmailTemplate('mail_template/mail_template.php');
+$template->hoten = $thongtinkhachhang[2];
+$template->email = $thongtinkhachhang[0];
+$template->diachi = $thongtinkhachhang[3] . ', ' . $thongtinkhachhang[4];
+$template->sodienthoai = $thongtinkhachhang[5];
+$template->soluongvedat = $dondathang[2];
+$template->giave = $giave;
+$template->loaive = $dondathang[3];
+$template->tongtien = $dondathang[1];
+$template->madonhang = $MADH;
+
+$template->ngaydatve = 'Ngày ' . date('d', $ngaydatve) . ' Tháng ' . date('m', $ngaydatve) . ' Năm ' . date('Y', $ngaydatve) . ' '. date('H:i:s', $ngaydatve);
+
+$body = $template->compile();
 
 $mail->Body = $body;
 
---
+++ mail_template/mail_template.php
@@ -0,0 +1,218 @@
+﻿<table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" bgcolor="#dcf0f8" style="margin:0;padding:0;background-color:#f2f2f2;width:100%!important;font-family:Arial,Helvetica,sans-serif;font-size:12px;color:#444;line-height:18px">
+    <tbody>
+    <tr>
+        <td align="center" valign="top" style="font-family:Arial,Helvetica,sans-serif;font-size:12px;color:#444;line-height:18px;font-weight:normal">
+
+            
+            <table border="0" cellpadding="0" cellspacing="0" width="600" style="margin-top:15px">
+                <tbody>
+                <tr>
+                    <td align="center" valign="bottom">
+                        <table width="100%" cellpadding="0" cellspacing="0" style="border-bottom:3px solid #00b7f1;padding:10px 0;background-color:#fff">
+                            <tbody>
+                            <tr>
+                                <td valign="top" bgcolor="#FFFFFF" width="30%" style="padding:0">
+                                    <a style="border:medium none; text-decoration:none; color:#007ed3; margin:0px 120px 0px 20px" href="http://saigonpolarexpo.com.vn" target="_blank">
+                                        <img src="<?php echo "http://$_SERVER[HTTP_HOST]"; ?>/img/logo.png" style="width: 120px" class="CToWUd">
+                                    </a>
+                                </td>
+								<td>
+									<p style="color: #02acea; font-size: 35px; font-weight: bold; margin: 0;">Saigon Polar Expo</p>
+									<p style="color: #02acea; font-size: 12px; font-weight: bold; padding: 10px 5px 0 20px; margin: 0; text-align: right;">Đơn vị phân phối vé trực tiếp: Công ty TNHH MTV Dịch Vụ và Tư Vấn</p>
+									<p style="color: #02acea; font-size: 20px; font-weight: bold; padding: 10px 5px 0 0; margin: 0; text-align: right;">Tân Đạt Thuận</p>
+								</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+
+
+                
+                <tr style="background:#fff">
+                    <td align="left" width="600" height="auto" style="padding:15px">
+                        <table>
+                            <tbody>
+                            <tr>
+
+                                <td>
+                                    <h1 style="font-size:17px;font-weight:bold;color:#444;padding:0 0 5px 0;margin:0">
+                                        Cảm ơn quý khách <?php echo $hoten; ?> đã đặt hàng tại saigonpolarexpo.com.vn,</h1>
+
+										<p style="margin:4px 0;font-family:Arial,Helvetica,sans-serif;font-size:12px;color:#444;line-height:18px;font-weight:normal">
+                                        <b>Tân Đạt Thuận</b> rất vui thông báo đơn hàng  <b>#<?php echo $madonhang; ?></b> của quý khách đã được tiếp nhận và đang trong quá trình 
+xử lý. <b>Tân Đạt Thuận</b> sẽ thông báo đến quý khách ngay khi hàng chuẩn bị được giao. 
+                                    </p>                                   
+                                    <h3 style="font-size:13px;font-weight:bold;color:#02acea;text-transform:uppercase;margin:20px 0 0 0;border-bottom:1px solid #ddd">
+                                        Thông tin đơn hàng #<?php echo $madonhang; ?> <span style="font-size:12px;color:#777;text-transform:none;font-weight:normal">(<?php echo $ngaydatve; ?>)</span>
+                                    </h3>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="font-family:Arial,Helvetica,sans-serif;font-size:12px;color:#444;line-height:18px">
+
+                                    <table cellspacing="0" cellpadding="0" border="0" width="100%">
+                                        <thead>
+                                        <tr>
+                                            <th align="left" width="50%" style="padding:6px 9px 0px 9px;font-family:Arial,Helvetica,sans-serif;font-size:12px;color:#444;font-weight:bold">
+                                                Thông tin thanh toán
+                                            </th>
+                                            <th align="left" width="50%" style="padding:6px 9px 0px 9px;font-family:Arial,Helvetica,sans-serif;font-size:12px;color:#444;font-weight:bold">
+                                                                                                Địa chỉ giao hàng
+                                                                                            </th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+
+                                        <tr>
+                                            <td valign="top" style="padding:3px 9px 9px 9px;border-top:0;font-family:Arial,Helvetica,sans-serif;font-size:12px;color:#444;line-height:18px;font-weight:normal">
+                                                <span style="text-transform:capitalize"><?php echo $hoten; ?></span><br>
+                                                <a href="mailto:<?php echo $email; ?>" target="_blank"><?php echo $email; ?></a><br>
+                                                <?php echo $sodienthoai ?>
+                                            </td>
+
+                                            <td valign="top" style="padding:3px 9px 9px 9px;border-top:0;border-left:0;font-family:Arial,Helvetica,sans-serif;font-size:12px;color:#444;line-height:18px;font-weight:normal">
+                                                <span style="text-transform:capitalize">
+                                                <?php echo $hoten; ?></span><br>
+
+                                                                                                <a href="mailto:<?php echo $email; ?>" target="_blank"><?php echo $email; ?></a><br>
+                                                <?php echo $diachi; ?><br>
+                                                
+                                                T: <?php echo $sodienthoai ?>
+                                            </td>
+                                        </tr>
+
+                                        
+                                        <tr>
+                                            <td valign="top" style="padding:7px 9px 0px 9px;border-top:0;font-family:Arial,Helvetica,sans-serif;font-size:12px;color:#444" colspan="2">
+                                                <p style="font-family:Arial,Helvetica,sans-serif;font-size:12px;color:#444;line-height:18px;font-weight:normal">
+
+                                                    <strong>Phương thức thanh toán: </strong>
+                                                    Thanh toán tiền mặt khi nhận hàng
+                                                    <br>
+
+                                                                                                        <strong>Thời
+                                                        gian giao
+                                                        hàng dự
+                                                        kiến:</strong>
+                                                                                                        Dự kiến giao hàng sau 2 – 3 ngày làm việc.
+                                                                                                        <br>
+                                                    <strong>Phí vận chuyển: </strong>
+                                                    0.000&nbsp;₫
+                                                    <br>
+                                                </p>
+                                            </td>
+                                        </tr>
+
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td>
+                                    <h2 style="text-align:left;margin:10px 0;border-bottom:1px solid #ddd;padding-bottom:5px;font-size:13px;color:#02acea">
+                                        CHI TIẾT ĐƠN HÀNG</h2>
+
+                                    <table cellspacing="0" cellpadding="0" border="0" width="100%" style="background:#f5f5f5">
+                                        <thead>
+                                        <tr>
+                                            <th align="left" bgcolor="#02acea" style="padding:6px 9px;color:#fff;font-family:Arial,Helvetica,sans-serif;font-size:12px;line-height:14px">Sản phẩm</th>
+                                            <th align="left" bgcolor="#02acea" style="padding:6px 9px;color:#fff;font-family:Arial,Helvetica,sans-serif;font-size:12px;line-height:14px"> Đơn giá</th>
+                                            <th align="left" bgcolor="#02acea" style="padding:6px 9px;color:#fff;font-family:Arial,Helvetica,sans-serif;font-size:12px;line-height:14px">Số lượng</th>
+                                            <th align="left" bgcolor="#02acea" style="padding:6px 9px;color:#fff;font-family:Arial,Helvetica,sans-serif;font-size:12px;line-height:14px">Giảm giá</th>
+                                            <th align="right" bgcolor="#02acea" style="padding:6px 9px;color:#fff;font-family:Arial,Helvetica,sans-serif;font-size:12px;line-height:14px">Tổng tạm</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody bgcolor="#eee" style="font-family:Arial,Helvetica,sans-serif;font-size:12px;color:#444;line-height:18px">
+											<tr>
+												<td align="left" valign="top" style="padding:3px 9px">
+													<span>Voucher tham quan <?php echo $loaive == 'Ngày thường' ? 'ngày thường' : 'ngày chủ nhật – lễ'; ?></span><br>
+																								</td>
+												<td align="left" valign="top" style="padding:3px 9px"><span><?php echo $giave ?>&nbsp;₫</span></td>
+                                                                                                <td align="left" valign="top" style="padding:3px 9px"><?php echo $soluongvedat; ?></td>
+												<td align="left" valign="top" style="padding:3px 9px"><span>0&nbsp;₫</span></td>
+												<td align="right" valign="top" style="padding:3px 9px"><span><?php echo $tongtien; ?>&nbsp;₫</span></td>
+											</tr>
+										</tbody>
+                                        <tfoot style="font-family:Arial,Helvetica,sans-serif;font-size:12px;color:#444;line-height:18px">
+                                        <tr>
+                                            <td colspan="4" align="right" style="padding:5px 9px">Tổng giá trị sản phẩm chưa giảm</td>
+                                            <td align="right" style="padding:5px 9px"><span><?php echo $tongtien; ?>&nbsp;₫</span></td>
+                                        </tr>
+
+                                        <tr>
+                                            <td colspan="4" align="right" style="padding:5px 9px">Giảm giá </td>
+                                            <td align="right" style="padding:5px 9px"><span>0&nbsp;₫</span></td>
+                                        </tr>
+
+                                                                                <tr>
+                                            <td colspan="4" align="right" style="padding:5px 9px">Chi phí vận chuyển</td>
+                                            <td align="right" style="padding:5px 9px"><span>0.000&nbsp;₫</span></td>
+                                        </tr>
+                                        
+                                        <tr bgcolor="#eee">
+                                            <td colspan="4" align="right" style="padding:7px 9px"><strong><big>Tổng giá trị đơn hàng</big></strong></td>
+                                            <td align="right" style="padding:7px 9px"><strong><big><span><?php echo $tongtien; ?>&nbsp;₫</span></big></strong></td>
+                                        </tr>
+                                        </tfoot>
+                                    </table>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td>
+                                    <br>
+                                    <p style="margin:0;font-family:Arial,Helvetica,sans-serif;font-size:12px;color:#444;line-height:18px;font-weight:normal">
+                                        Trường hợp quý khách có những băn khoăn về đơn hàng hoặc cần hỗ trợ thông tin khác. Vui lòng liên hệ số điện thoại <font style="color: green; font-weight: bold">08.6654.6410 - 08.6654.6412 - 08.6654.6413 </font> ( giờ hành chính) hoặc email <a href="mailto:saigon.polarexpo@gmail.com"><font style="color: green; font-weight: bold">saigon.polarexpo@gmail.com</font></a>. Đội ngũ <b>Tân Đạt Thuận</b> luôn sẵn sàng hỗ trợ bạn bất kì lúc nào. 
+                                    </p>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td>
+                                    <br>
+
+                                    <p style="font-family:Arial,Helvetica,sans-serif;font-size:12px;margin:0;padding:0;line-height:18px;color:#444;font-weight:bold">
+                                        Một lần nữa Saigon Polar Expo cảm ơn quý khách.<br>
+                                    </p>
+
+                                    <p style="font-family:Arial,Helvetica,sans-serif;font-size:12px;color:#444;line-height:18px;font-weight:normal;text-align:right">
+										<strong><a style="color:#00a3dd;text-decoration:none;font-size:13px">Đơn vị phân phối vé trực tiếp Công ty TNHH MTV Dịch Vụ và Tư Vấn Tân Đạt Thuận</a></strong><br>
+                                    </p>
+                                </td>
+                            </tr>
+
+
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+
+
+                </tbody>
+
+            </table>
+
+
+        </td>
+
+    </tr>
+
+    <tr>
+        <td align="center">
+            <table width="600">
+                <tbody><tr>
+                    <td>
+                        <p style="font-family:Arial,Helvetica,sans-serif;font-size:11px;line-height:18px;color:#4b8da5;padding:10px 0;margin:0px;font-weight:normal" align="left">
+                            Quý khách nhận được email này vì đã mua hàng tại saigonpolarexpo.com.<br>
+                            Để chắc chắn luôn nhận được email thông báo, xác nhận mua hàng từ saigonpolarexpo.com, quý khách vui lòng thêm địa chỉ <strong><a href="mailto:saigon.polarexpo@gmail.com" target="_blank">saigon.polarexpo@gmail.com</a></strong> vào số địa chỉ (Address Book, Contacts) của hộp email.<br>
+                            <b>Văn phòng Tân Đạt Thuận:</b> Số 4 Út Tịch, Phường 4, Quận Tân Bình, Tp.Hô Chí Minh.
+                        </p>
+                    </td>
+                </tr>
+            </tbody></table>
+        </td>
+    </tr>
+    </tbody>
+</table>
